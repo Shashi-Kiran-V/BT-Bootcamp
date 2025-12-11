70470871bd8bd9146f423db30ae81988bb3adb50
Shashi-Kiran-V <shashikiranshetty02@gmail.com>
2025-12-11 12:22:03 +0530

    implemented challenge 23 partially

---
+++ coding-challenges-bootcamp/04-Loops-and-Iteration/challenge_23.py
@@ -0,0 +1,19 @@
+try:
+    # Take user input
+    n = int(input("Enter the value of N: "))
+    
+    # Validate input
+    if n <= 0:
+        print("Please enter a positive number.")
+    else:
+        # Display the series
+        print(f"\nSeries up to {n}:")
+        num = 1
+        
+        while num <= n:
+            print(num, end=" ")
+            num += 4
+        print()  # New line after series
+
+except ValueError:
+    print("Invalid input. Please enter a valid integer.")
